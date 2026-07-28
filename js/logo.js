bb4258e11bcd94b9238b9e43b209ffc06f78524b
imanol2314 <imanollarico75@gmail.com>
2026-07-27 21:54:59 -0400

    proyecto acabado

--- js/logo.js
+++ js/logo.js
@@ -1,14 +1,9 @@
-// Cambiar a Iniciar Sesión
 function mostrarLogin() {
   document.getElementById("formLogin").style.display = "block";
   document.getElementById("formRegistro").style.display = "none";
-  
-  // Cambia la pestaña activa
   document.getElementById("btnTabLogin").classList.add("activa");
   document.getElementById("btnTabRegistro").classList.remove("activa");
 }
-
-// Cambiar a Registrarse
 function mostrarRegistro() {
   document.getElementById("formLogin").style.display = "none";
   document.getElementById("formRegistro").style.display = "block";
@@ -18,7 +13,6 @@ function mostrarRegistro() {
   document.getElementById("btnTabLogin").classList.remove("activa");
 }
 
-// Validar Ingreso
 function validarLogin() {
   let user = document.getElementById("loginUsuario").value;
   let pass = document.getElementById("loginPassword").value;
@@ -30,8 +24,6 @@ function validarLogin() {
     window.location.href = "index.html"; // Redirige al inicio
   }
 }
-
-// Validar Registro
 function validarRegistro() {
   let nom = document.getElementById("regNombre").value;
   let user = document.getElementById("regUsuario").value;
